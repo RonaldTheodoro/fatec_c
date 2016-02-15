5d1b699ae85d16d31711d607ca3419d9c0fc78e2
Ronald Theodoro Nascimento Silva <ronaldtheodoro@gmail.com>
2016-02-15 08:53:53 -0200

    Create mediav2.cpp

---
+++ mediav2.cpp
@@ -0,0 +1,23 @@
+#include "iostream"
+#include "cstdlib"
+#include "math.h"
+
+using namespace std;
+
+
+int main () {
+    float no1, no2, media;
+    
+    system ("cls");
+    cout << "Programa media!" << endl;
+    cout << "Digite nota 1: ";
+    cin >> no1;
+    cout << "Digite nota 2: ";
+    cin >> no2;
+    media = (no1 + no2) / 2;
+
+    cout << "Media: " << media << endl;
+    system ("pause");
+    
+    return 0;    
+}
