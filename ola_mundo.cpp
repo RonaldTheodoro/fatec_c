f35dc9a40e3bc7b4479088a883416de099e1259d
Ronald Theodoro Nascimento Silva <ronaldtheodoro@gmail.com>
2016-02-15 08:36:00 -0200

    Rename media.cpp to ola_mundo.cpp

---
+++ ola_mundo.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "cstdlib"
+#include "math.h"
+
+using namespace std;
+
+float no1 = 4, no2 = 5;
+
+
+int main () {
+    system ("cls");
+    cout << "Ola mundo!" << endl;
+    cout << "Media:  " << (no1 + no1) / 2 << endl;
+    system ("pause");
+    return 0;
+}
